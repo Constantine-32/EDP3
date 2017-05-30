ecb7024a769b62fa8a8835ca87dc3f1c459b5556
Christian C. Romero <id.32@outlook.com>
2017-05-30 18:40:57 +0200

    Clean up Main code.

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -3,46 +3,63 @@ package Application;
 import DataStructures.*;
 
 import java.io.*;
+import java.text.Normalizer;
 import java.util.Scanner;
 
 public class Main {
   // Objecte Scanner per obtenir la informacio de la consola.
   private static Scanner keyboard = new Scanner(System.in);
-  //
-  private static TADVocabulary vocabulary;
-
+  // Retorna una copia de l'string pasada per parametre que tant sols conte caracters [a-z] i espais.
+  private static String normalizeString(final String string) {
+    return Normalizer.normalize(string, Normalizer.Form.NFD).
+            replaceAll("[\\p{InCombiningDiacriticalMarks}]", "").
+            replaceAll("[,.'\"(){}!¡?¿[0-9]]", "").
+            replaceAll("’", " ").
+            toLowerCase();
+  }
   // Main del programa.
   public static void main(String[] args) {
-    vocabulary = new Trie();
+    TADVocabulary vocabulary = new Trie();
+//    vocabulary.add("home");
+//    vocabulary.add("homeworkers");
+//    vocabulary.add("homework");
+//    vocabulary.add("mountain");
 
-    Main.vocabulary.add("home");
-    Main.vocabulary.add("homeworkers");
-    Main.vocabulary.add("homework");
-    Main.vocabulary.add("mountain");
+    String filePath = "Text1.txt";
+//    System.out.println("Indica el nom del fitxer:");
+//    filePath = keyboard.nextLine();
 
-    BufferedReader file = null;
-    String filePath;
-    boolean valid = false;
-    while (!valid) {
-//      System.out.println("Indica el nom del fitxer:");
-//      filePath = keyboard.nextLine();
-      filePath = "Text1.txt";
-      try {
-        file = new BufferedReader(new FileReader(new File(filePath)));
-        valid = true;
-      } catch (FileNotFoundException e) {
-        System.out.println("No s'ha trobat el fitxer indicat.");
-      }
-    }
+    try (BufferedReader file = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "Cp1252"))) {
+      String line;
+      int numPlana = 1;
+      int numLinia = 1;
 
-    String line;
-    try {
       while ((line = file.readLine()) != null) {
         if (line.matches("<Plana numero=\\d+>")) {
-          System.out.println(line);
+          numPlana = Integer.parseInt(line.replaceAll(".*(\\d+).*", "$1"));
+          numLinia = 1;
+        } else {
+          line = normalizeString(line);
+          Scanner scanner = new Scanner(line);
+          scanner.useDelimiter(" ");
+          while (scanner.hasNext()) {
+            String word = scanner.next();
+            if (word.charAt(0) == '$') {
+              vocabulary.add(word.substring(1, word.length()));
+              System.out.println(numPlana+":"+numLinia+":"+word.substring(1, word.length()));
+            } else {
+              if (vocabulary.contains(word)) {
+                System.out.println(numPlana+":"+numLinia+":"+word);
+              }
+            }
+          }
+          numLinia++;
         }
       }
-      file.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("No s'ha trobat el fitxer indicat.");
+    } catch (UnsupportedEncodingException e) {
+      System.out.println("L'archiu utilitza una codificacio no esperada.");
     } catch (IOException e) {
       System.out.println(e.toString());
     }
