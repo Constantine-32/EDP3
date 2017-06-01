4611670d1ec6d9f129545009099b85adeee01517
Christian C. Romero <id.32@outlook.com>
2017-06-01 23:52:21 +0200

    Finished Hash class.

--- src/DataStructures/Hash.java
+++ src/DataStructures/Hash.java
@@ -1,68 +1,7 @@
 package DataStructures;
 
-import java.util.Iterator;
-import java.util.StringJoiner;
-
-public class Hash implements TADVocabulary {
-
+public class Hash extends GenericHashSet<String> implements ADTVocabulary {
   public Hash() {
-
-  }
-
-  @Override
-  public boolean add(String word) {
-    return false;
-  }
-
-  @Override
-  public boolean remove(String word) {
-    return false;
-  }
-
-  @Override
-  public boolean contains(String word) {
-    return false;
-  }
-
-  @Override
-  public boolean prefix(String prefix) {
-    return false;
-  }
-
-  @Override
-  public boolean isEmpty() {
-    return false;
-  }
-
-  private class TADIterator implements Iterator<String> {
-
-    private TADIterator() {
-
-    }
-
-    @Override
-    public String next() {
-      return null;
-    }
-
-    @Override
-    public boolean hasNext() {
-      return false;
-    }
-  }
-
-  @Override
-  public Iterator<String> iterator() {
-    return new TADIterator();
-  }
-
-  @Override
-  public String toString() {
-    if (isEmpty()) return "[]";
-    StringJoiner sj = new StringJoiner(", ", "[", "]");
-    for (String word : this) {
-      sj.add(word);
-    }
-    return sj.toString();
+    super();
   }
 }
